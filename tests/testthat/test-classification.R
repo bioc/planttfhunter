b76fc3061584bc08c5f565df6464314d8f46be92
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-03-12 13:28:15 +0100

    Fixed unit test for classify_tfs()

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -4,7 +4,7 @@ data(gsu_annotation)
 domain_annotation <- gsu_annotation
 
 #----Start tests----------------------------------------------------------------
-test_that("multiplication works", {
+test_that("classify_tfs() returns a 2-column data frame", {
   families <- classify_tfs(domain_annotation)
   expect_equal(class(families), "data.frame")
   expect_equal(ncol(families), 2)
