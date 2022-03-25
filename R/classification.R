a9f73af3083a1c0176815dfae7bb6413984e4bc4
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-03-25 11:19:21 +0100

    Bug fix: classify_tfs() was failing when it could not assign TFs to any family

--- R/classification.R
+++ R/classification.R
@@ -14,24 +14,29 @@
 #' domain_annotation <- gsu_annotation
 #' families <- classify_tfs(domain_annotation)
 classify_tfs <- function(domain_annotation = NULL) {
-  dom <- list_domains(show = "dbd")
-  annot <- domain_annotation[domain_annotation$Domain %in% dom, ]
-  annot_list <- split(annot, annot[, 1])
-  
-  fams <- Reduce(rbind, lapply(annot_list, function(x) {
-    domains <- x$Domain
+    dom <- list_domains(show = "dbd")
+    annot <- domain_annotation[domain_annotation$Domain %in% dom, ]
+    annot_list <- split(annot, annot[, 1])
     
-    # Apply all helper classification functions to object domains
-    funcs <- list(check_ap2_erf, check_b3, check_c2c2, check_garp,
-                  check_hb, check_mads, check_myb, check_nf_y, check_smallfams)
-    tffam <- unlist(lapply(funcs, function(f) f(domains)))
-    fam_df <- data.frame(
-      Gene = unique(x[, 1]),
-      Family = tffam
-    )
-    return(fam_df)
-  }))
-  return(fams)
+    fams <- Reduce(rbind, lapply(annot_list, function(x) {
+        domains <- x$Domain
+        
+        # Apply all helper classification functions to object domains
+        funcs <- list(check_ap2_erf, check_b3, check_c2c2, check_garp,
+                      check_hb, check_mads, check_myb, check_nf_y, check_smallfams)
+        tffam <- unlist(lapply(funcs, function(f) f(domains)))
+        if(length(tffam) == 0) {
+            fam_df <- NULL
+        } else {
+            fam_df <- data.frame(
+                Gene = unique(x[, 1]),
+                Family = tffam
+            )
+            
+        }
+        return(fam_df)
+    }))
+    return(fams)
 }
 
 
